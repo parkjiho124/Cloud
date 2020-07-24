e84fbd19969d60117a45171d51cc5bc7ae903dd2
parkjiho124 <62838393+parkjiho124@users.noreply.github.com>
2020-07-25 01:59:20 +0900

    Add files via upload

--- 2.ino
+++ 2.ino
@@ -1,34 +1,65 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <DHT.h>
-#define DHTPIN D4
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+#define DHTPIN D1
 #define DHTTYPE DHT11
- 
+
+#include <DHT.h>
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal_I2C lcd(0*27,16,2);
+ 
+// Set these to run example.
+#define FIREBASE_HOST "mydhtsensor-6b684.firebaseio.com"
+#define FIREBASE_AUTH "JPUmJOaFAOKvGZFqV3OTPb5WYO2MHmfwykQs7iFC"
+#define WIFI_SSID "201912739"
+#define WIFI_PASSWORD "jh010124"
  
 void setup() {
-  Serial.begin(115200);
-  dht.begin();
-  lcd.begin();
-  lcd.backlight();
+  Serial.begin(9600);
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  dht.begin(9600);
+  
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+   
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
  
 void loop() {
   float temp = dht.readTemperature();
   float humi = dht.readHumidity();
-  Serial.print("Temp: ");
-  Serial.print(temp);
-  Serial.print("   Humi: ");
+  Serial.print("Temperature = ");
+  Serial.println(temp);
+  Serial.print("Humidity = ");
   Serial.println(humi);
  
-  lcd.setCursor(0,0);
-  lcd.print("Temp: ");
-  lcd.print(temp);
- 
-  lcd.setCursor(0,1);
-  lcd.print("Humi: ");
-  lcd.println(humi);
- 
-  delay(1000);
+//  Firebase.setFloat("temperature", temp);  
+//  Firebase.setFloat("humidity", humi);    
+
+//  Firebase.pushFloat("temperature", temp);  
+//  Firebase.pushFloat("humidity", humi);       
+
+  StaticJsonBuffer<200> jsonbuffer;
+  JsonObject& root = jsonbuffer.createObject();
+  root["temperature"] = temp;
+  root["humidity"] = humi;
+
+  String name = Firebase.push("logDHT", root);
+  
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("pushing /logs failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  Serial.print("pushed: /logDHT/");
+  Serial.println(name);
+  
+  delay(5000);
 }
