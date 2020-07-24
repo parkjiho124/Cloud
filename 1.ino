ff8f77f02da50166b4e1023b1084d3e388ecb55e
parkjiho124 <62838393+parkjiho124@users.noreply.github.com>
2020-07-25 01:58:55 +0900

    Add files via upload

--- 1.ino
+++ 1.ino
@@ -1,20 +1,51 @@
-#include <DHT.h>
-#define DHTPIN D4
+#include <ESP8266WiFi.h>
+#include <FirebaseArduio.h>
+#define DHTPIN D2
 #define DHTTYPE DHT11
- 
+#include <DHT.h>
+
 DHT dht(DHTPIN, DHTTYPE);
- 
+
+#define FIREBASE_HOST "mydhtsensor-6b684.firebaseio.com"
+#define FIREBASE_AUTH "JPUmJOaFAOKvGZFqV3OTPb5WYO2MHmfwykQs7iFC"
+#define WIFI_SSID "201912739"
+#define WIFI_PASSWORD "jh010124"
+
 void setup() {
-  Serial.begin(115200);
-  dht.begin();
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  dht.begin(9600);
+
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
- 
+
 void loop() {
   float temp = dht.readTemperature();
   float humi = dht.readHumidity();
-  Serial.print("Temp: ");
-  Serial.print(temp);
-  Serial.print("   Humi: ");
+  Serial.print("Temperature = ");
+  Serial.println(temp);
+  Serial.print("Humidity = ");
   Serial.println(humi);
-  delay(1000);
+
+  Firebase.setFloat("temperature", temp);
+  Firebase.setFloat("humidity", humi);
+
+  if (Firebase.failed()) {
+    Serial.print("pushing /logs failed:");
+    Serial.println(Firebase.error());
+    return;
+  }
+  Serial.println("sent data to firebase");
+  delay(5000);
+  }
+
 }
